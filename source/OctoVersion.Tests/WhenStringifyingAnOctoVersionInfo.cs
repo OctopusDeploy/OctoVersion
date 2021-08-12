12ab9a8d725243a36984d330a19b4a08628cbb39
Matt Richardson <matt.richardson@octopus.com>
2021-08-12 14:03:04 +1000

    Make pre release tag and build metadata match semver2 spec
    We were allowing underscores and slashes in these fields, which is not supported according to the spec https://semver.org/spec/v2.0.0.html.

--- source/OctoVersion.Tests/WhenStringifyingAnOctoVersionInfo.cs
+++ source/OctoVersion.Tests/WhenStringifyingAnOctoVersionInfo.cs
@@ -24,7 +24,7 @@ namespace OctoVersion.Tests
             yield return new object[] { VersionWithPreReleaseTag, "-pre", "it should append the pre-release tag" };
             yield return new object[] { VersionWithPreReleaseTagAndBuildMetadata, "-pre", "it should append the pre-release tag, but not the build metadata" };
             yield return new object[] { VersionWithLongPreReleaseTagAndBuildMetadata, "-mark-genericDocumentStore", "it should add the pre-release tag, and ignore the build metadata" };
-            yield return new object[] { VersionWithLongPreReleaseTagWithSlashesAndBuildMetadata, "-dependabot/npm_and_yarn/source/TentacleArmy.Web/ini-1.3.6", "it should add the pre-release tag, and ignore the build metadata" };
+            yield return new object[] { VersionWithLongPreReleaseTagWithSlashesAndBuildMetadata, "-dependabot-npm-and-yarn-source-TentacleArmy.Web-ini-1.3.6", "it should add the pre-release tag, and ignore the build metadata" };
         }
 
         [Theory]
@@ -60,7 +60,7 @@ namespace OctoVersion.Tests
             yield return new object[] { VersionWithPreReleaseTag, "", "there is no build metadata" };
             yield return new object[] { VersionWithPreReleaseTagAndBuildMetadata, "+build", "it should return the build metadata" };
             yield return new object[] { VersionWithLongPreReleaseTagAndBuildMetadata, "+Branch.mark-genericDocumentStore.Sha.fb13016f3a21d7c2058fb74ab25f19e5311c6550", "it should return the build metadata" };
-            yield return new object[] { VersionWithLongPreReleaseTagWithSlashesAndBuildMetadata, "+Branch.dependabot/npm_and_yarn/source/TentacleArmy.Web/ini-1.3.6.Sha.069392d9d2d37ddb6009998b92e70963badcc666", "it should return the build metadata" };
+            yield return new object[] { VersionWithLongPreReleaseTagWithSlashesAndBuildMetadata, "+Branch.dependabot-npm-and-yarn-source-TentacleArmy.Web-ini-1.3.6.Sha.069392d9d2d37ddb6009998b92e70963badcc666", "it should return the build metadata" };
         }
 
         [Theory]
@@ -78,7 +78,7 @@ namespace OctoVersion.Tests
             yield return new object[] { VersionWithPreReleaseTag, "1.2.3-pre", "it should return the major.minor.patch and pre-release" };
             yield return new object[] { VersionWithPreReleaseTagAndBuildMetadata, "1.2.3-pre", "it should return the major.minor.patch and pre-release but not the build metadata" };
             yield return new object[] { VersionWithLongPreReleaseTagAndBuildMetadata, "2021.1.3-mark-genericDocumentStore", "it should return the major.minor.patch and pre-release but not the build metadata" };
-            yield return new object[] { VersionWithLongPreReleaseTagWithSlashesAndBuildMetadata, "2021.1.3-dependabot/npm_and_yarn/source/TentacleArmy.Web/ini-1.3.6", "it should return the major.minor.patch and pre-release but not the build metadata" };
+            yield return new object[] { VersionWithLongPreReleaseTagWithSlashesAndBuildMetadata, "2021.1.3-dependabot-npm-and-yarn-source-TentacleArmy.Web-ini-1.3.6", "it should return the major.minor.patch and pre-release but not the build metadata" };
         }
 
         [Theory]
@@ -96,7 +96,7 @@ namespace OctoVersion.Tests
             yield return new object[] { VersionWithPreReleaseTag, "1.2.3-pre", "it should append the pre-release tag" };
             yield return new object[] { VersionWithPreReleaseTagAndBuildMetadata, "1.2.3-pre", "it should return the major.minor.patch and pre-release but not the build metadata" };
             yield return new object[] { VersionWithLongPreReleaseTagAndBuildMetadata, "2021.1.3-mark-genericDocumen", "it should return the major.minor.patch and trim the pre-release to 20 chars" };
-            yield return new object[] { VersionWithLongPreReleaseTagWithSlashesAndBuildMetadata, "2021.1.3-dependabot/npm-and-", "it should return the major.minor.patch and trim the pre-release to 20 chars" };
+            yield return new object[] { VersionWithLongPreReleaseTagWithSlashesAndBuildMetadata, "2021.1.3-dependabot-npm-and-", "it should return the major.minor.patch and trim the pre-release to 20 chars" };
         }
 
         [Theory]
@@ -114,7 +114,7 @@ namespace OctoVersion.Tests
             yield return new object[] { VersionWithPreReleaseTag, "1.2.3-pre", "it should append the pre-release tag" };
             yield return new object[] { VersionWithPreReleaseTagAndBuildMetadata, "1.2.3-pre+build", "it should return the major.minor.patch and pre-release and build metadata" };
             yield return new object[] { VersionWithLongPreReleaseTagAndBuildMetadata, "2021.1.3-mark-genericDocumentStore+Branch.mark-genericDocumentStore.Sha.fb13016f3a21d7c2058fb74ab25f19e5311c6550", "it should return the major.minor.patch and pre-release and build metadata" };
-            yield return new object[] { VersionWithLongPreReleaseTagWithSlashesAndBuildMetadata, "2021.1.3-dependabot/npm_and_yarn/source/TentacleArmy.Web/ini-1.3.6+Branch.dependabot/npm_and_yarn/source/TentacleArmy.Web/ini-1.3.6.Sha.069392d9d2d37ddb6009998b92e70963badcc666", "it should return the major.minor.patch and pre-release and build metadata" };
+            yield return new object[] { VersionWithLongPreReleaseTagWithSlashesAndBuildMetadata, "2021.1.3-dependabot-npm-and-yarn-source-TentacleArmy.Web-ini-1.3.6+Branch.dependabot-npm-and-yarn-source-TentacleArmy.Web-ini-1.3.6.Sha.069392d9d2d37ddb6009998b92e70963badcc666", "it should return the major.minor.patch and pre-release and build metadata" };
         }
 
         [Theory]
