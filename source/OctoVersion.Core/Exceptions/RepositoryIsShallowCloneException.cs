fd406bd2ad685f48e624c6d9292a54ab84e81048
Matt Richardson <matt.richardson@octopus.com>
2022-02-10 10:55:57 +1100

    Throw a nice error if it's a shallow clone (#56)
    If it's a shallow clone, then we cant look through the history to determine the version number

---
+++ source/OctoVersion.Core/Exceptions/RepositoryIsShallowCloneException.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace OctoVersion.Core.Exceptions
+{
+    public class RepositoryIsShallowCloneException : ControlledFailureException
+    {
+        public RepositoryIsShallowCloneException(string message) : base(message)
+        {
+        }
+
+        public RepositoryIsShallowCloneException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
