e3210a0a59051fcc0b83f2bd5925e7cebdf23a60
TeamCity <bob@octopus.com>
2022-08-24 23:58:54 +0000

    Run ReSharper code cleanup

--- source/Cake.OctoVersion/Logging/CakeSink.cs
+++ source/Cake.OctoVersion/Logging/CakeSink.cs
@@ -4,43 +4,42 @@ using Cake.Core.Diagnostics;
 using Serilog.Core;
 using Serilog.Events;
 
-namespace Cake.OctoVersion.Logging
+namespace Cake.OctoVersion.Logging;
+
+public class CakeSink : ILogEventSink
 {
-    public class CakeSink : ILogEventSink
+    readonly ICakeContext context;
+
+    public CakeSink(ICakeContext context)
     {
-        readonly ICakeContext context;
+        this.context = context;
+    }
 
-        public CakeSink(ICakeContext context)
+    public void Emit(LogEvent logEvent)
+    {
+        var message = logEvent.MessageTemplate.Render(logEvent.Properties);
+        var errorDetails = logEvent.Exception?.ToString() ?? string.Empty;
+
+        switch (logEvent.Level)
         {
-            this.context = context;
-        }
-
-        public void Emit(LogEvent logEvent)
-        {
-            var message = logEvent.MessageTemplate.Render(logEvent.Properties);
-            var errorDetails = logEvent.Exception?.ToString() ?? string.Empty;
-
-            switch (logEvent.Level)
-            {
-                case LogEventLevel.Verbose:
-                    context.Log.Verbosity = Verbosity.Verbose;
-                    context.Log.Information(message);
-                    break;
-                case LogEventLevel.Debug:
-                case LogEventLevel.Information:
-                    context.Log.Information(message);
-                    break;
-                case LogEventLevel.Warning:
-                    context.Log.Warning(message);
-                    break;
-                case LogEventLevel.Error:
-                case LogEventLevel.Fatal:
-                    context.Log.Error(message);
-                    context.Log.Error(errorDetails);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(logEvent.Level), logEvent.Level, null);
-            }
+            case LogEventLevel.Verbose:
+                context.Log.Verbosity = Verbosity.Verbose;
+                context.Log.Information(message);
+                break;
+            case LogEventLevel.Debug:
+            case LogEventLevel.Information:
+                context.Log.Information(message);
+                break;
+            case LogEventLevel.Warning:
+                context.Log.Warning(message);
+                break;
+            case LogEventLevel.Error:
+            case LogEventLevel.Fatal:
+                context.Log.Error(message);
+                context.Log.Error(errorDetails);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(logEvent.Level), logEvent.Level, null);
         }
     }
 }
