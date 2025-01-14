d8e0e027a860845852b438252da3c0790fb45114
borland <orion.edwards@gmail.com>
2025-01-14 14:27:32 +1300

    Fix build

--- build/Build.cs
+++ build/Build.cs
@@ -29,7 +29,7 @@ class Build : NukeBuild
 
     [Parameter("Whether to auto-detect the branch name - this is okay for a local build, but should not be used under CI.")] readonly bool AutoDetectBranch = IsLocalBuild;
 
-    [OctoVersion(BranchMember = nameof(BranchName), AutoDetectBranchMember = nameof(AutoDetectBranch), Framework = "net6.0")]
+    [OctoVersion(BranchMember = nameof(BranchName), AutoDetectBranchMember = nameof(AutoDetectBranch), Framework = "net8.0")]
     public OctoVersionInfo OctoVersionInfo;
 
     [Parameter("Branch name for OctoVersion to use to calculate the version number. Can be set via the environment variable " + CiBranchNameEnvVariable + ".", Name = CiBranchNameEnvVariable)]
